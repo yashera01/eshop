391f3283f04f75c12f0eea91ed348e199ab12d42
Yash <yashtotale95@gmail.com>
2023-10-31 10:36:15 +0530

    first commit

---
+++ src/components/About/about.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about-page">
+      <h1>About Us</h1>
+      <p>
+        Welcome to our website! We are a company dedicated to providing
+        high-quality products and excellent services to our customers.
+      </p>
+      <p>
+        Our mission is to make your life better by offering innovative
+        solutions that meet your needs.
+      </p>
+      <p>
+        Feel free to explore our products and contact us if you have any
+        questions or need assistance.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="contact-page">
+      <h1>Contact Us</h1>
+      <p>
+        If you have any questions or need assistance, please feel free to
+        contact us using the information below.
+      </p>
+      <div className="contact-details">
+        <p>Email: contact@example.com</p>
+        <p>Phone: +1 (123) 456-7890</p>
+        <p>Address: 123 Main Street, City, Country</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
